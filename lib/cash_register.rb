b6d3d306f2957addd851a5403d4366f68e5787d8
Oscar Abuto Achieng <oscarachieng94@gmail.com>
2022-08-31 14:28:50 +0300

    commit tree c0954774a728328e725573b9e4bb7528a3267dc4

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,44 @@
+class CashRegister 
+    attr_accessor :total
+    @@total = [] 
+    def initialize(*employee_discount) 
+        @total = 0
+        @employee_discount = employee_discount
+    end
 
+    def total 
+        @total
+    end
+
+    def add_item (title, price, *quantity)
+        
+        @total += price.to_f*quantity.to_f
+       
+        @total
+    end
+
+    def apply_discount 
+       if employee_discount > 0 
+          discount = @total*(employee_discount/100).to_f 
+          @total = @total - discount 
+          @@total << @total
+          "Your latest total Price is : #{@total}" 
+       else 
+         "No discount to apply"
+       end        
+    end
+
+    def items (name, price, *quantity)
+        if quantity
+            quantity.times do 
+                [name]
+            end
+        else
+            [name]
+        end
+    end
+
+ def void_last_transaction 
+     @@total.pop.sum
+ end
+end
